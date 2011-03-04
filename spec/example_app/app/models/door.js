2efc4c3f9bd836213a1d3cf6f60ad78ecd0f716e
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-03-04 09:54:22 -0500

    more tweaks

--- spec/example_app/app/models/door.js
+++ spec/example_app/app/models/door.js
@@ -1,8 +1,3 @@
 var Door = Jax.Model.create({
-  after_initialize: function() { },
-
-  /* user clicks on the door, so they need to go to the next scene */
-  onclick: function(evt) {
-    redirect_to(this.destination);
-  }
+  after_initialize: function() { }
 });
