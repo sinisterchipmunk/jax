ff984430e805ee14b4976cb192f8cbc6993ab73a
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-03-04 07:38:55 -0500

    added some more constants

--- lib/jax/version.rb
+++ lib/jax/version.rb
@@ -1,3 +1,12 @@
 module Jax
-  VERSION = "0.0.1"
+  module Version
+    MAJOR = 0
+    MINOR = 0
+    TINY  = 1
+    PATCH = 0
+
+    STRING = PATCH == 0 ? "#{MAJOR}.#{MINOR}.#{TINY}" : "#{MAJOR}.#{MINOR}.#{TINY}.#{PATCH}"
+  end
+
+  VERSION = Version::STRING
 end
