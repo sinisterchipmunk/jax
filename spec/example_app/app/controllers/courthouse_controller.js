20ddc3311b46b662f4e808275880b028d96baeec
Colin <colin@itchy.(none)>
2011-03-04 09:54:22 -0500

    more tweaks

--- spec/example_app/app/controllers/courthouse_controller.js
+++ spec/example_app/app/controllers/courthouse_controller.js
@@ -1,5 +1,5 @@
 var CourthouseController = Jax.Controller.create(ApplicationController, {
-  /* 'index' action. Sets up the scene. called by default when first
+  /* 'index' action. Sets up the scene. 'index' is called by default when first
      switching to this controller, unless another action name is given.
   */
   index: function($super) {
@@ -7,6 +7,16 @@ var CourthouseController = Jax.Controller.create(ApplicationController, {
     World.addLightSource(LightSource.find('sun'));
     World.addObject(Scene.find('courthouse'));
     World.addObject(Character.find('judge'));
+  },
+
+  /* Some special actions are fired whenever the corresponding input is
+     received from the user.
+   */
+  mouse_clicked: function(event) {
+    if (event.target.isKindOf(Door)) {
+      /* user clicks on the door, so they need to go to the next scene */
+      redirect_to(event.target.destination);
+    }
   },
 
   /* clean up the scene. Special function which is called when switching
