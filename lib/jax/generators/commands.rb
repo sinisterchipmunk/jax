a328fcd575bbde00fc2541e3e889f71b2fcbd4c4
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-03-05 13:30:30 -0500

    Model generator

--- lib/jax/generators/commands.rb
+++ lib/jax/generators/commands.rb
@@ -3,7 +3,8 @@ require 'thor/group'
 
 module Jax
   module Generators
-    autoload :App, File.join(File.dirname(__FILE__), "app/app_generator")
+    autoload :App,        File.join(File.dirname(__FILE__), "app/app_generator")
     autoload :Controller, File.join(File.dirname(__FILE__), "controller/controller_generator")
+    autoload :Model,      File.join(File.dirname(__FILE__), "model/model_generator")
   end
 end
