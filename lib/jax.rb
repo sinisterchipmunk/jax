941ef14fba2137deb2367de0a903482350fe1994
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-03-05 15:59:15 -0500

    added Jax::Application so rake doesn't fail

--- lib/jax.rb
+++ lib/jax.rb
@@ -1,5 +1,6 @@
 module Jax
-  autoload :Generators, File.join(File.dirname(__FILE__), "jax/generators/commands")
-  autoload :VERSION, File.join(File.dirname(__FILE__), "jax/version")
-  autoload :Version, File.join(File.dirname(__FILE__), "jax/version")
+  autoload :Generators,  File.join(File.dirname(__FILE__), "jax/generators/commands")
+  autoload :VERSION,     File.join(File.dirname(__FILE__), "jax/version")
+  autoload :Version,     File.join(File.dirname(__FILE__), "jax/version")
+  autoload :Application, File.join(File.dirname(__FILE__), "jax/application")
 end
---
+++ lib/jax/application.rb
@@ -0,0 +1,5 @@
+module Jax
+  class Application
+    
+  end
+end
