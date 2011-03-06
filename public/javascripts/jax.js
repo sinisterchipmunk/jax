9af538f274f89f1d548e022d6a73fe4abe270f5b
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-03-06 18:56:03 -0500

    Refactoring

--- public/javascripts/jax.js
+++ public/javascripts/jax.js
@@ -28,6 +28,40 @@ var Jax = (function() {
     throw new Error("Could not find Jax element!");
   }
   
+  function checkJaxLoaded() {
+    for (var p in loaded_files) {
+      if (loaded_files[p] != 1) {
+        Jax.loaded = false;
+        return;
+      }
+    }
+    Jax.loaded = true;
+  }
+  
+  function onScriptReady(script, callback) {
+    // Opera has readyState too, but does not behave in a consistent way
+    if (script.readyState && script.onload !== null) {
+      // IE only (onload===undefined) not Opera (onload===null)
+      script.onreadystatechange = function() {
+        if (script.readyState === "loaded" || script.readyState === "complete" ) {
+          // Avoid memory leaks (and duplicate call to callback) in IE
+          script.onreadystatechange = null;
+          callback();
+        }
+      };
+    } else {
+      // other browsers (DOM Level 0)
+      script.onload = function() { callback(); }
+    }
+  }
+  
+  function newScriptElement(path) {
+    var script = document.createElement("script");
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute("src", path);
+    return script;
+  }
+
   var jaxScriptElement = getJaxScriptElement();
   var base_path = getJaxBasePath();
   var loaded_files = {};
@@ -38,47 +72,10 @@ var Jax = (function() {
     /* don't load the same file twice */
     if (loaded_files[path] == 1) return;
 
-    /* don't load this file unless it's Prototype, or unless Prototype has already been loaded */
-    if (path != base_path+'/prototype.js' && typeof(Prototype) == "undefined")
-    {
-      Jax.after_prototype_loaded(function() {
-        
-      });
-      return;
-    }
-
-    
-    var script = document.createElement("script");
-    script.setAttribute("type", "text/javascript");
-    script.setAttribute("src", path);
     loaded_files[path] = 0;
+    var script = newScriptElement(path);
     
-    function checkJaxLoaded() {
-      loaded_files[path] = 1;
-      for (var p in loaded_files) {
-        if (loaded_files[p] == 0) {
-          Jax.loaded = false;
-          return;
-        }
-      }
-      Jax.loaded = true;
-    }
-
-    // Opera has readyState too, but does not behave in a consistent way
-    if (script.readyState && script.onload !== null) {
-      // IE only (onload===undefined) not Opera (onload===null)
-      script.onreadystatechange = function() {
-        if (script.readyState === "loaded" || script.readyState === "complete" ) {
-          // Avoid memory leaks (and duplicate call to callback) in IE
-          script.onreadystatechange = null;
-          if (callback) callback();
-          checkJaxLoaded(path);
-        }
-      };
-    } else {
-      // other browsers (DOM Level 0)
-      script.onload = function() { if (callback) callback(); checkJaxLoaded(); }
-    }
+    onScriptReady(script, function() { if (callback) callback(); loaded_files[path] = 1; checkJaxLoaded(); });
     
     /*
       we want to insert the script after the last script to have been required, OR after *this* script
