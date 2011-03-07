e2b76c0731217518750e7c0a65e59aaa56ecdeee
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-03-06 22:10:10 -0500

    Jax::Application::Configuration and part of a JS view compiler

--- lib/jax.rb
+++ lib/jax.rb
@@ -3,4 +3,19 @@ module Jax
   autoload :VERSION,     File.join(File.dirname(__FILE__), "jax/version")
   autoload :Version,     File.join(File.dirname(__FILE__), "jax/version")
   autoload :Application, File.join(File.dirname(__FILE__), "jax/application")
+  autoload :Packager,    File.join(File.dirname(__FILE__), "jax/packager")
+  
+  class << self
+    def application
+      @application ||= nil
+    end
+    
+    def application=(application)
+      @application = application
+    end
+    
+    def root
+      application && application.root
+    end
+  end
 end
