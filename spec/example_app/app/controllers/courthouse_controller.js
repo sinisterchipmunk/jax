da4b96f110e0c29b91bd59f59dc0aec2839af185
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-03-08 14:11:02 -0500

    example app tweaks

--- spec/example_app/app/controllers/courthouse_controller.js
+++ spec/example_app/app/controllers/courthouse_controller.js
@@ -6,9 +6,9 @@ var CourthouseController = (function() {
        switching to this controller, unless another action name is given.
      */
     index: function() {
-      World.addLightSource(LightSource.find('sun'));
-      World.addObject(Scene.find('courthouse'));
-      World.addObject(Character.find('judge'));
+      this.world.addLightSource(LightSource.find('sun'));
+      this.world.addObject(Scene.find('courthouse'));
+      this.world.addObject(Character.find('judge'));
     },
 
     /* Some special actions are fired whenever the corresponding input is
@@ -17,7 +17,7 @@ var CourthouseController = (function() {
     mouse_clicked: function(event) {
       if (event.target.isKindOf(Door)) {
         /* user clicks on the door, so they need to go to the next scene */
-        redirect_to(event.target.destination);
+        this.redirect_to(event.target.destination);
       }
     },
 
