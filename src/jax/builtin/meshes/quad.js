f76f9b7c2733ae5582b39305d12b44ce032fa521
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-03-14 09:05:24 -0400

    Controller invocation delegates world and player from context; documentation for quads

--- src/jax/builtin/meshes/quad.js
+++ src/jax/builtin/meshes/quad.js
@@ -1,3 +1,16 @@
+/**
+ * class Jax.Mesh.Quad < Jax.Mesh
+ * 
+ * A simple square or rectangle. You can adjust its width and height, and that's about it.
+ * 
+ * This mesh is generally used for testing purposes, or for simple, textured objects like smoke particles.
+ *
+ * Examples:
+ * 
+ *     var quad = new Jax.Mesh.Quad({width: 2, height: 1});
+ *     var quad = new Jax.Mesh.Quad(1.5);
+ * 
+ **/
 Jax.Mesh.Quad = (function() {
   return Jax.Class.create(Jax.Mesh, {
     initialize: function($super, options) {
@@ -7,11 +20,27 @@ Jax.Mesh.Quad = (function() {
       
       this.setSize(options && options.width || 1, options && options.height || 1)
     },
-    
+
+    /**
+     * Jax.Mesh.Quad#setWidth(width) -> undefined
+     * - width (Number): the width of this quad in WebGL Units.
+     * Sets the width of this quad.
+     **/
     setWidth: function(width) { this.setSize(width, this.height); },
     
+    /**
+     * Jax.Mesh.Quad#setHeight(height) -> undefined
+     * - height (Number): the height of this quad in WebGL Units.
+     * Sets the height of this quad.
+     **/
     setHeight:function(height){ this.setHeight(this.width, height); },
     
+    /**
+     * Jax.Mesh.Quad#setSize(width, height) -> undefined
+     * - width (Number): the width of this quad in WebGL Units.
+     * - height (Number): the height of this quad in WebGL Units.
+     * Sets the width and height of this quad.
+     **/
     setSize: function(width, height) {
       this.width = width;
       this.height = height;
