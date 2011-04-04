8a91c4d2e286212b9e436503ca87940811dc61c0
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-04-03 21:02:36 -0400

    Bugfix: Jax.Quad should initialize with no arguments (use default options)
    Added no-options tests for all built-in meshes

---
+++ spec/javascripts/jax/builtin/quad_spec.js
@@ -0,0 +1,8 @@
+describe("Jax.Mesh.Quad", function() {
+  var quad;
+  
+  describe("with no options", function() {
+    beforeEach(function() { quad = new Jax.Mesh.Quad(); });
+    it("should initialize", function() { });
+  });
+});
---
+++ spec/javascripts/jax/builtin/sphere_spec.js
@@ -0,0 +1,8 @@
+describe("Jax.Mesh.Sphere", function() {
+  var sphere;
+  
+  describe("with no options", function() {
+    beforeEach(function() { sphere = new Jax.Mesh.Sphere(); });
+    it("should initialize", function() { });
+  });
+});
