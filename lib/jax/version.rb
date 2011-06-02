1c0aa45910884c516af0daf31a183a4634af43c2
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-06-02 18:30:42 -0400

    Bumped version to 0.0.0.9

--- lib/jax/version.rb
+++ lib/jax/version.rb
@@ -3,7 +3,7 @@ module Jax
     MAJOR = 0
     MINOR = 0
     TINY  = 0
-    PATCH = 8
+    PATCH = 9
 
     STRING = PATCH == 0 ? "#{MAJOR}.#{MINOR}.#{TINY}" : "#{MAJOR}.#{MINOR}.#{TINY}.#{PATCH}"
   end
