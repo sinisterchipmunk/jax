cdaaaf6ecdcb3fe542bdda67e70621487014b054
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-06-28 15:29:17 -0400

    Gracefully stop shadowmap generation with a warning if it results in errors

--- src/jax/webgl/scene/light_manager.js
+++ src/jax/webgl/scene/light_manager.js
@@ -94,11 +94,11 @@ Jax.Scene.LightManager = (function() {
       options = Jax.Util.normalizeOptions(options, {});
       
       // Use alpha blending for the first pass, and additive blending for subsequent passes.
-      this.context.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+      context.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
       for (var i = 0; i < this._lights.length; i++) {
         this._current_light = i;
         this._lights[i].render(context, this.objects, options);
-        if (i == 0) this.context.glBlendFunc(GL_ONE, GL_ONE);
+        if (i == 0) context.glBlendFunc(GL_ONE, GL_ONE);
       }
       
       delete this._current_light;
