1601f27e570842892d002fd218e8a2c13f2dec64
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-07-18 12:37:13 -0400

    Version bump to 1.0.1.rc2

--- lib/jax/version.rb
+++ lib/jax/version.rb
@@ -3,7 +3,7 @@ module Jax
     MAJOR = 1
     MINOR = 0
     TINY  = 1
-    PREREL= "rc1"
+    PREREL= "rc2"
 
     STRING = PREREL == 0 ? "#{MAJOR}.#{MINOR}.#{TINY}" : "#{MAJOR}.#{MINOR}.#{TINY}.#{PREREL}"
   end
