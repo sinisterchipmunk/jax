8451a5265e84614f0282bfe1e2eb0f92326599e0
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-07-27 14:10:33 -0400

    Changelog updates for v1.0.1

--- lib/jax/version.rb
+++ lib/jax/version.rb
@@ -1,11 +1,11 @@
 module Jax
   module Version
-    MAJOR = 1
-    MINOR = 1
-    TINY  = 0
-    PREREL= "rc1"
+    MAJOR  = 1
+    MINOR  = 1
+    TINY   = 0
+    PREREL = "rc1"
 
-    STRING = PREREL == 0 ? "#{MAJOR}.#{MINOR}.#{TINY}" : "#{MAJOR}.#{MINOR}.#{TINY}.#{PREREL}"
+    STRING = PREREL ? "#{MAJOR}.#{MINOR}.#{TINY}.#{PREREL}" : "#{MAJOR}.#{MINOR}.#{TINY}"
   end
 
   VERSION = Version::STRING
