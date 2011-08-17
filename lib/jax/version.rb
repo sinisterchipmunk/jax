4aa6f112b13733c463de59a144cf28ee53d2de7e
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-08-17 12:33:32 -0400

    Update version number to 1.1.1.rc1

--- lib/jax/version.rb
+++ lib/jax/version.rb
@@ -2,8 +2,8 @@ module Jax
   module Version
     MAJOR  = 1
     MINOR  = 1
-    TINY   = 0
-    PREREL = nil 
+    TINY   = 1
+    PREREL = "rc1"
 
     STRING = PREREL ? "#{MAJOR}.#{MINOR}.#{TINY}.#{PREREL}" : "#{MAJOR}.#{MINOR}.#{TINY}"
   end
