a76b7dab64fb9ed174d15d0e0d3c9afa1247df13
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-08-22 11:04:17 -0400

    Disabled error raised when max texture count is exceeded. See commit.

--- src/jax/webgl/shader/manifest.js
+++ src/jax/webgl/shader/manifest.js
@@ -40,7 +40,13 @@ Jax.Shader.Manifest = Jax.Class.create({
   texture: function(name, tex, context) {
     if (!context) throw new Error("Can't bind texture without a context");
     if (this.texture_tracker == GL_MAX_ACTIVE_TEXTURES-1) {
-      throw new Error("Maximum number of textures ("+GL_MAX_ACTIVE_TEXTURES+") has been reached!");
+      /* FIXME add a callback for Materials to enter compatibility mode and recompile their shaders
+         before throwing a hard error. Until this is implemented, the hard error itself is disabled
+         because it causes unnecessary barfing on machines that are otherwise capable of running the
+         scene. Using too many textures doesn't seem to have any catastrophic results on tested
+         hardware, except some visual anomolies, so there's no reason to crash out catastrophically.
+      */
+//      throw new Error("Maximum number of textures ("+GL_MAX_ACTIVE_TEXTURES+") has been reached!");
     }
     
     if (typeof(this.getValue(name)) != "number")
