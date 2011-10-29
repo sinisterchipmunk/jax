6ed433517efedb1d14ff2a4148e522055d6d30cb
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-10-28 22:27:51 -0400

    helper generator does not generate redundant names
    ex: user_helper instead of user_helper_helper

--- lib/generators/jax/helper/helper_generator.rb
+++ lib/generators/jax/helper/helper_generator.rb
@@ -5,12 +5,25 @@ module Jax
     class HelperGenerator < Jax::Generators::NamedBase
       def create_helper
         coffee_template_with_fallback "helper.js",
-          File.join('app/assets/jax/helpers', "#{file_name}_helper.js")
+          File.join('app/assets/jax/helpers', "#{underscored_helper_name}.js")
       end
 
       def create_spec
         coffee_template_with_fallback "helper_spec.js",
-          File.join('spec/javascripts/jax/helpers', "#{file_name}_helper_spec.js")
+          File.join('spec/javascripts/jax/helpers', "#{underscored_helper_name}_spec.js")
+      end
+      
+      protected
+      def underscored_helper_name
+        if file_name =~ /_helper$/
+          file_name
+        else
+          "#{file_name}_helper"
+        end
+      end
+
+      def camelized_helper_name
+        underscored_helper_name.camelize
       end
     end
   end
--- spec/generators/jax/helper_generator_spec.rb
+++ spec/generators/jax/helper_generator_spec.rb
@@ -20,4 +20,36 @@ describe 'jax:helper' do
       subject.should generate("spec/javascripts/jax/helpers/user_input_helper_spec.js")
     end
   end
+  
+  with_args "user_input_helper" do
+    it "should generate 'user_input_helper' and not 'user_input_helper_helper'" do
+      subject.should generate("app/assets/jax/helpers/user_input_helper.js.coffee") { |c|
+        c.should     match(/UserInputHelper/)
+        c.should_not match(/UserInputHelperHelper/)
+      }
+      subject.should generate("spec/javascripts/jax/helpers/user_input_helper_spec.js.coffee") { |c|
+        c.should     match(/UserInputHelper/)
+        c.should_not match(/UserInputHelperHelper/)
+      }
+
+      subject.should_not generate("app/assets/jax/helpers/user_input_helper_helper.js.coffee")
+      subject.should_not generate("spec/javascripts/jax/helpers/user_input_helper_helper_spec.js.coffee")
+    end
+    
+    with_args "--without-coffeescript" do
+      it "should generate 'user_input_helper' and not 'user_input_helper_helper'" do
+        subject.should generate("app/assets/jax/helpers/user_input_helper.js") { |c|
+          c.should     match(/UserInputHelper/)
+          c.should_not match(/UserInputHelperHelper/)
+        }
+        subject.should generate("spec/javascripts/jax/helpers/user_input_helper_spec.js") { |c|
+          c.should     match(/UserInputHelper/)
+          c.should_not match(/UserInputHelperHelper/)
+        }
+
+        subject.should_not generate("app/assets/jax/helpers/user_input_helper_helper.js")
+        subject.should_not generate("spec/javascripts/jax/helpers/user_input_helper_helper_spec.js")
+      end
+    end
+  end
 end
