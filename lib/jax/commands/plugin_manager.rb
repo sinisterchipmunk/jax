e37792caca3508293cf202d76f81307d82958a2b
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-10-31 23:15:34 -0400

    Encoding and IO fixes for 1.9.2

--- lib/jax/commands/plugin_manager.rb
+++ lib/jax/commands/plugin_manager.rb
@@ -234,7 +234,9 @@ module Jax
               destination_directory = File.dirname(destination_file)
               FileUtils.mkdir_p destination_directory unless File.directory?(destination_directory)
               File.open destination_file, "w" do |f|
-                f.print tarfile.read
+                binary = tarfile.read
+                binary.force_encoding "UTF-8" if binary.respond_to?(:force_encoding)
+                f.print binary
               end
             end
           end
@@ -261,7 +263,10 @@ module Jax
       gz = StringIO.new("")
       z = Zlib::GzipWriter.new(gz)
       z.write tarfile.string
-      gz.rewind
+      z.close
+      
+      # zlib.close is required, but closes gz so we need a new gz
+      gz = StringIO.new gz.string
       
       # this is probably bad practice (?) but it seems so silly
       # to create a whole class for just this accessor.
