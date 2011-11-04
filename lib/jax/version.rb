ea8a248f6f2f56c408a665c261bc5dcd0e06df59
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-11-04 11:28:30 -0400

    bump version to 2.0.1

--- lib/jax/version.rb
+++ lib/jax/version.rb
@@ -2,7 +2,7 @@ module Jax
   module Version
     MAJOR = 2
     MINOR = 0
-    PATCH = 0
+    PATCH = 1
     BUILD = nil
     STRING = BUILD ? [MAJOR, MINOR, PATCH, BUILD].join(".") : [MAJOR, MINOR, PATCH].join(".")
   end
