5837c34fa9016ce58f4c5bfb97325156da7cc7f4
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2012-06-06 15:28:42 -0400

    Finally removed the legacy "Lighting" shader
    This shader has been replaced by combining the LambertDiffuse,
    PhongSpecular and Attenuation shaders.

--- lib/assets/javascripts/shaders/lighting/common.glsl
+++
@@ -1,9 +0,0 @@
-//= require "shaders/functions/lights"
-
-shared uniform bool LIGHTING_ENABLED;
-shared uniform mat4 ivMatrix, mvMatrix;
-shared uniform mat3 vnMatrix, nMatrix;
-shared uniform float materialShininess;
-
-shared varying vec3 vLightDir;
-shared varying vec3 vNormal, vSurfacePos;
--- lib/assets/javascripts/shaders/lighting/vertex.glsl
+++
@@ -1,9 +0,0 @@
-shared attribute vec2 VERTEX_TEXCOORDS;
-shared attribute vec3 VERTEX_NORMAL;
-shared attribute vec4 VERTEX_POSITION, VERTEX_COLOR, VERTEX_TANGENT;
-
-void main(void) {
-  vNormal = nMatrix * VERTEX_NORMAL;
-  vSurfacePos = (mvMatrix * VERTEX_POSITION).xyz;
-  vLightDir = normalize(vnMatrix * -normalize(LIGHT_DIRECTION));
-}
