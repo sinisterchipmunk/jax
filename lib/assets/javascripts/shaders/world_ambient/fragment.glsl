ae0231c346de3fed9dcbcc7fe94f01cc2b18a629
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2012-06-10 18:27:33 -0400

    fix bug in world ambient layer
    just FYI, this bug was caught by the UNIT TESTS written for
    the Jax SHADERS. So the shader simuator was worth the effort
    of implementing, and my fairly-strict unit testing discipline
    has once again been proven right. Boo-yah.

--- lib/assets/javascripts/shaders/world_ambient/fragment.glsl
+++ lib/assets/javascripts/shaders/world_ambient/fragment.glsl
@@ -1,6 +1,6 @@
 void main(void) {
   if (PASS == 0) {
     // ambient pass only
-    gl_FragColor.rgb *= vColor.rgb * WorldAmbientColor.rgb * WorldAmbientColor.a * MaterialAmbientIntensity;
+    gl_FragColor.rgb *= WorldAmbientColor.rgb * WorldAmbientColor.a * MaterialAmbientIntensity;
   }
 }
