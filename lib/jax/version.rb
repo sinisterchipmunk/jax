4430625734c9d568bee42e1692d461b5b17d7d04
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2012-09-21 12:33:29 -0400

    bump version to 3.0.0.rc1
    We're almost there....

--- lib/jax/version.rb
+++ lib/jax/version.rb
@@ -1,9 +1,9 @@
 module Jax
   module Version
-    MAJOR = 2
+    MAJOR = 3
     MINOR = 0
-    PATCH = 8
-    BUILD = nil
+    PATCH = 0
+    BUILD = "rc1"
     STRING = BUILD ? [MAJOR, MINOR, PATCH, BUILD].join(".") : [MAJOR, MINOR, PATCH].join(".")
   end
   
