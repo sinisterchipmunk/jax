abcd720d5434baf748130c7b39486abececea32e
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2012-10-28 20:52:21 -0400

    return listener instead of its index, remove obsolete spec

--- spec/javascripts/jax/core/events_spec.js
+++ spec/javascripts/jax/core/events_spec.js
@@ -38,17 +38,4 @@ describe("Jax.EventEmitter", function() {
     emitter.fireEvent('evt', {});
     expect(evt).toBeUndefined(); // because the original listener never was fired
   });
-  
-  it("should be un-listenable out of order", function() {
-    // add a second listener. Its index is 1. If we remove index 0, we should still
-    // be able to remove index 1. At that point no listeners should exist.
-    var second = false, secondID;
-    secondID = emitter.addEventListener('evt', function() { second = true; });
-    emitter.removeEventListener('evt', listenerID);
-    emitter.removeEventListener('evt', secondID);
-    emitter.fireEvent('evt', {});
-    // neither should have fired
-    expect(second).toBeFalsy();
-    expect(evt).toBeUndefined();
-  });
 });
