f9059e0e9f90c306aa9f1847bf46e212cbd28eb1
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2013-02-03 09:03:13 -0500

    move matrix tests, which are really camera tests, to camera spec

--- spec/javascripts/jax/camera_spec.js
+++ spec/javascripts/jax/camera_spec.js
@@ -2,6 +2,90 @@ describe("Jax.Camera", function() {
   var camera, control;
   
   beforeEach(function() { camera = new Jax.Camera(); });
+
+  describe("after translating forward", function() {
+    beforeEach(function() {
+      camera.move(10);
+    });
+
+    it("should have proper direction vectors", function() {
+      expect(camera.direction).toEqualVector([0,0,-1]);
+      expect(camera.up).toEqualVector([0,1,0]);
+      expect(camera.right).toEqualVector([1,0,0]);
+    });
+
+    describe("yawing 90 deg", function() {
+      beforeEach(function() { camera.yaw(Math.deg2rad(90)); });
+      
+      it("should be facing 'right'", function() {
+        expect(camera.direction).toEqualVector([-1,0,0]);
+      });
+      
+      it("should have up vector 'up'", function() {
+        expect(camera.up).toEqualVector([0,1,0]);
+      });
+      
+      it("should have right vector 'forward'", function() {
+        expect(camera.right).toEqualVector([0,0,-1]);
+      });
+      
+      it("should produce expected quat", function() {
+        expect(camera.rotation).toEqualVector([0,0.707106,0,0.707106]);
+      });
+      
+      it("should produce expected mat4", function() {
+        expect(camera.getTransformationMatrix()).toEqualVector([0,0,-1,0,0,1,0,0,1,0,0,0,0,0,-10,1]);
+      });
+    });
+    
+    describe("pitching 90 deg", function() {
+      beforeEach(function() { camera.pitch(Math.deg2rad(90)); });
+      
+      it("should be facing 'up'", function() {
+        expect(camera.direction).toEqualVector([0,1,0]);
+      });
+      
+      it("should have up vector 'forward'", function() {
+        expect(camera.up).toEqualVector([0,0,1]);
+      });
+      
+      it("should have right vector 'right'", function() {
+        expect(camera.right).toEqualVector([1,0,0]);
+      });
+      
+      it("should produce expected quat", function() {
+        expect(camera.rotation).toEqualVector([0.707106,0,0,0.707106]);
+      });
+      
+      it("should produce expected mat4", function() {
+        expect(camera.getTransformationMatrix()).toEqualVector([1,0,0,0,0,0,1,0,0,-1,0,0,0,0,-10,1]);
+      });
+    });
+
+    describe("rolling 90 deg", function() {
+      beforeEach(function() { camera.roll(Math.deg2rad(90)); });
+      
+      it("should be facing 'forward'", function() {
+        expect(camera.direction).toEqualVector([0,0,-1]);
+      });
+      
+      it("should have up vector 'left'", function() {
+        expect(camera.up).toEqualVector([1,0,0]);
+      });
+      
+      it("should have right vector 'down'", function() {
+        expect(camera.right).toEqualVector([0,-1,0]);
+      });
+      
+      it("should produce expected quat", function() {
+        expect(camera.rotation).toEqualVector([0,0,-0.707106,0.707106]);
+      });
+      
+      it("should produce expected mat4", function() {
+        expect(camera.getTransformationMatrix()).toEqualVector([0,-1,0,0,1,0,0,0,0,0,1,0,0,0,-10,1]);
+      });
+    });
+  });
   
   describe("its frustum", function() {
     var frustum;
