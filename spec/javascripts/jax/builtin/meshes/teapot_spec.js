7c725e96f0907091b06a75eb9c2a81547b61c65a
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2013-03-18 20:43:59 -0400

    translate to coffee

--- spec/javascripts/jax/builtin/meshes/teapot_spec.js
+++
@@ -1,8 +0,0 @@
-describe("Jax.Mesh.Teapot", function() {
-  var teapot;
-  
-  describe("with no options", function() {
-    beforeEach(function() { teapot = new Jax.Mesh.Teapot(); });
-    it("should initialize", function() { });
-  });
-});
